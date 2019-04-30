7ab461e3247dbe391d6cc2ca20e7bf5702c3bd14
Guillaume Martin <guillaumemartin548@gmail.com>
2019-04-30 14:28:02 -0400

    ajout

--- Orion_modele.py
+++ Orion_modele.py
@@ -73,7 +73,7 @@ class Batiment(): #Ajouter le 8 avril par nic
         self.nom=""
         self.etat=""
 
-class TourDefense():   ### à ajouter git
+class TourDefense():   ### GM 29 avril
     def __init__(self,nom,plan,x,y):
         self.id=Id.prochainid()
         self.proprietaire=nom
